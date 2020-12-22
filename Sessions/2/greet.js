cb25cf753187c0bd7d47c9b543fbaf529b143fe7
Brandt <starshipbrandt@gmail.com>
2020-12-21 18:43:14 -0600

    Alert demo

--- Sessions/2/greet.js
+++ Sessions/2/greet.js
@@ -4,8 +4,7 @@ function greet(element)
     let name = document.getElementById("name_field").value;
     if (name == "")
     {
-        document.getElementById("output_field").value = "Please enter a name.";
-        //alert("Please enter a name.");
+        alert("Please enter a name.");
     } else {
         //alert("Greetings, " + name + "!");
         document.getElementById("output_field").value = "Greetings, " + name + "!";
